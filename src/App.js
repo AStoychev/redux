d09aa4ea4a0cce8e6d1f2021fc7947e1b9eae61b
Stoychev <stoychev.nas@gmail.com>
2023-12-07 16:47:31 +0200

    Task four from redux drilling field

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import PostList from "./features/posts/PostList";
 import AddPostForm from "./features/posts/AddPostForm";
+import SinglePostPage from "./features/posts/SinglePostPage";
+import EditPostForm from "./features/posts/EditPostForm";
+import Layout from "./components/Layout";
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
     return (
-        <div className="App">
-            <AddPostForm />
-            <PostList />
-        </div>
+        <Routes>
+            <Route path="/" element={<Layout />}>
+
+                <Route index element={<PostList />} />
+
+                <Route path="post">
+                    <Route index element={<AddPostForm />} />
+                    <Route path=":postId" element={<SinglePostPage />} />
+                    <Route path="edit/:postId" element={<EditPostForm />} />
+                </Route>
+
+            </Route>
+        </Routes>
     );
 }
 
--- src/features/posts/PostList.js
+++ src/features/posts/PostList.js
@@ -1,21 +1,13 @@
-import { useSelector, useDispatch } from "react-redux";
-import { selectAllPosts, getPostsStatus, getPostsError, fetchPosts } from "./postsSlice";
-import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { selectAllPosts, getPostsStatus, getPostsError } from "./postsSlice";
 
 import PostExcerpt from "./PostExcerpt";
 
 const PostList = () => {
-    const dispatch = useDispatch();
 
     const posts = useSelector(selectAllPosts);
     const postStatus = useSelector(getPostsStatus);
     const error = useSelector(getPostsError);
-
-    useEffect(() => {
-        if (postStatus === 'idle') {
-            dispatch(fetchPosts())
-        }
-    }, [postStatus, dispatch]);
 
     let content;
     if(postStatus === 'loading') {
@@ -29,7 +21,6 @@ const PostList = () => {
 
     return (
         <section>
-            <h2>Posts</h2>
             {content}
         </section>
     )
